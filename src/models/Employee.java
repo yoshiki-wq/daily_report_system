6f20ce7116aef58b38290c8bf475de65e7defd32
yoshiki-wq <yoishisk@gmail.com>
2020-08-14 09:17:31 +0900

    Add Employee class

---
+++ src/models/Employee.java
@@ -0,0 +1,125 @@
+package models;
+
+import java.sql.Timestamp;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+
+@Table(name = "employees")
+@NamedQueries({
+    @NamedQuery(
+            name = "getAllEmployees",
+            query = "SELECT e FROM Employee AS e ORDER BY e.id DESC"
+            ),
+    @NamedQuery(
+            name = "getEmployeesCount",
+            query = "SELECT COUNT(e) FROM Employee AS e"
+            ),
+    @NamedQuery(
+            name = "checkRegisteredCode",
+            query = "SELECT COUNT(e) FROM Employee AS e WHERE e.code = :code"
+            ),
+    @NamedQuery(
+            name = "checkLoginCodeAndPassword",
+            query = "SELECT e FROM Employee AS e WHERE e.delete_flag = 0 AND e.code = :code AND e.password = :pass"
+            )
+})
+@Entity
+public class Employee {
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "code", nullable = true, unique = true)
+    private Integer code;
+
+    @Column(name = "name", nullable = true)
+    private String name;
+
+    @Column(name = "password", length = 64, nullable = true)
+    private String password;
+
+    @Column(name = "admin_flag", nullable = true)
+    private Integer admin_flag;
+
+    @Column(name = "create_at", nullable = true)
+    private Timestamp create_at;
+
+    @Column(name = "update_at", nullable = true)
+    private Timestamp updata_at;
+
+    @Column(name = "delete_flag", nullable = true)
+    private Integer delete_flag;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getCode() {
+        return code;
+    }
+
+    public void setCode(Integer code) {
+        this.code = code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Integer getAdmin_flag() {
+        return admin_flag;
+    }
+
+    public void setAdmin_flag(Integer admin_flag) {
+        this.admin_flag = admin_flag;
+    }
+
+    public Timestamp getCreate_at() {
+        return create_at;
+    }
+
+    public void setCreate_at(Timestamp create_at) {
+        this.create_at = create_at;
+    }
+
+    public Timestamp getUpdata_at() {
+        return updata_at;
+    }
+
+    public void setUpdata_at(Timestamp updata_at) {
+        this.updata_at = updata_at;
+    }
+
+    public Integer getDelete_flag() {
+        return delete_flag;
+    }
+
+    public void setDelete_flag(Integer delete_flag) {
+        this.delete_flag = delete_flag;
+    }
+
+}
